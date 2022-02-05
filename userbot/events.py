90e35ba19a59085716f5a2ff0ee1fe74cfdde851
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-02-06 00:43:35 +0800

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -96,11 +96,11 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Geez-Project ERROR**\n"
-                    link = "Silahkan chat: @VckyouuBitch"
+                    text = "**WIKI-UBOT ERROR**\n"
+                    link = "Silahkan chat: @Wiki_W"
                     text += "Untuk melaporkan kesalahan"
                     text += f"tinggal teruskan pesan ini {link}.\n"
-                    text += "Vcky Siap Membantu Kamu\n"
+                    text += "Wiki Siap Membantu Kamu\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
